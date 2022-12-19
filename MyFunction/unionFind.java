1365a147c0949dd10eafee799cb7e3a86e3b3406
dzf0023 <dzf0023@auburn.edu>
2022-12-19 16:37:08 -0600

    Update unionFind.java

--- MyFunction/unionFind.java
+++ MyFunction/unionFind.java
@@ -1,36 +1,26 @@
 // UnionFind.class
 class UnionFind {
     private int[] root;
-    private int[] rank;
 
     public UnionFind(int size) {
         root = new int[size];
-        rank = new int[size];
         for (int i = 0; i < size; i++) {
             root[i] = i;
-            rank[i] = 1; 
         }
     }
 
     public int find(int x) {
-        while (x != root[x]) {
-            x = root[x];
+        if (x == root[x]) {
+            return x;
         }
-        return x;
+        return root[x] = find(root[x]);
     }
 
     public void union(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
         if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
+            root[rootY] = rootX;
         }
     }
 
